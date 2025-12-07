aaf4f70b62dee1bb3527b702665f3d22ddf55074
teguh.efriyanto <ext.efri@jalin.co.id>
2025-12-07 22:54:44 +0700

    chore: Remove REFERENSI folder from repository
    - Removed C++ reference files from tracking
    - Added REFERENSI/ to .gitignore
    - Reference files not needed in production repository

--- REFERENSI/ej_dialog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-
-// Panel handler functions for EJ Parser
-void HandleBrowseInput(HWND hPanel);
-void HandleProcessEJ(HWND hPanel);
-
-// Legacy dialog function (deprecated - use panel handlers)
-void ShowEJParserDialog(HWND hWndParent);
